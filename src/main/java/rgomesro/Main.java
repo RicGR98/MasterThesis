3c12c4665b7fcb5894b6103fe50cacad7224d6c2
Ricardo Gomes Rodrigues <ricardog@live.be>
2021-04-18 23:50:44 +0200

    Timing redone

--- src/main/java/rgomesro/Main.java
+++ src/main/java/rgomesro/Main.java
@@ -4,13 +4,11 @@ import rgomesro.models.World;
 
 public class Main {
     public static void main(String[] args){
-        long start = System.nanoTime();
+        long start = System.currentTimeMillis();
         World world = new World();
-        long startRun = System.nanoTime();
         world.run();
-        long end = System.nanoTime();
-        long denom = 1000000000;
-        System.out.println((startRun - start)/denom + " + " + (end - startRun)/denom + " = " + (end - start)/denom + "s");
         world.saveAllToCsv();
+        long end = System.currentTimeMillis();
+        System.out.println((end - start)/1000 + " seconds");
     }
 }
